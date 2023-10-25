c2a7b87933f481960672f91bd87dc49c7c7c7272
J4nis05 <janis.roelli@gmail.com>
2023-10-25 10:56:41 +0200

    Updated Install Script

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ sudo apt install zsh wget neofetch hollywood -y
 
 # Remove Any Pre Existing Docker Apps, Images and general configuration
 echo "Removing old Docker Confgiuration"
-sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo rm -rf /var/lib/docker
 sudo rm -rf /var/lib/containerd
@@ -15,7 +15,7 @@ sudo rm -rf /var/lib/containerd
 # Add Docker's official GPG key:
 echo "Installing Docker Daemon and Plug-Ins"
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/raspbian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -28,7 +28,7 @@ echo \
 sudo apt-get update
 
 # Install The Latest Version of Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Install Oh-My-ZSH
 echo "Installing Oh-My-ZSH"
