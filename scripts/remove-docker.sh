486ac73158b8303b8d29027062d975e3aa8aa46d
404 <janisroelli@gmx.ch>
2023-10-26 09:40:56 +0200

    Cleaned Up Scripts

---
+++ scripts/remove-docker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Remove Any Pre Existing Docker Apps, Images and general configuration
+echo "Removing old Docker Confgiuration"
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras -y
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo rm -rf /var/lib/docker
+sudo rm -rf /var/lib/containerd
+sudo rm -rf /docker
